58dd38c75776f5bef82dc0bc369378a29516d5f1
Mura Mihai <mihai.mura2@gmail.com>
2022-07-06 10:42:55 +0300

    2scripts

---
+++ src/main.cpp
@@ -0,0 +1,72 @@
+#include <Arduino.h>
+#include "Keyboard.h"
+
+// script pins
+#define SCRIPT1_GND 4
+#define SCRIPT1 5
+#define SCRIPT1_PWR 6
+#define SCRIPT2_GND 7
+#define SCRIPT2 8
+#define SCRIPT2_PWR 9
+
+void typeKey(int key)
+{
+  Keyboard.press(key);
+  delay(50);
+  Keyboard.release(key);
+}
+
+void setup()
+{
+  // failsafe
+  pinMode(2, INPUT);
+  pinMode(3, OUTPUT);
+  digitalWrite(3, HIGH);
+
+  // script1
+  pinMode(SCRIPT1_GND, OUTPUT);
+  pinMode(SCRIPT1, INPUT);
+  pinMode(SCRIPT1_PWR, OUTPUT);
+  digitalWrite(SCRIPT1_GND, LOW);
+  digitalWrite(SCRIPT1_PWR, HIGH);
+  // script2
+  pinMode(SCRIPT2_GND, OUTPUT);
+  pinMode(SCRIPT2, INPUT);
+  pinMode(SCRIPT2_PWR, OUTPUT);
+  digitalWrite(SCRIPT2_GND, LOW);
+  digitalWrite(SCRIPT2_PWR, HIGH);
+
+  delay(500);
+  // failsafe
+  if (digitalRead(2) == LOW)
+    return;
+
+  Keyboard.begin();
+
+  //* SCRIPT 1
+  if (digitalRead(SCRIPT1) == HIGH)
+  {
+    delay(500);
+
+    // INSERT SCRIPT 1 HERE
+
+    Keyboard.end();
+    return;
+  }
+  //* SCRIPT 2
+  if (digitalRead(SCRIPT2) == HIGH)
+  {
+    delay(500);
+
+    // INSERT SCRIPT 2 HERE
+
+    Keyboard.end();
+    return;
+  }
+
+  Keyboard.end();
+}
+
+void loop()
+{
+}
